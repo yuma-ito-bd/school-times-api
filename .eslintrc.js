fd35df9ad2f9984dbbc1ccea67e0880ef07f7eb7
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-03-01 23:04:48 +0900

    TypeScript, ESLint, Prettierの導入

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint",
+    ],
+    plugins: ["@typescript-eslint"],
+    parser: "@typescript-eslint/parser",
+}
