93f4e5f4596f1534e301772e3d8c6373585f612c
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-03-15 11:57:44 +0900

    学級だよりの作成、更新、削除APIを作成

--- src/routes/ArticlesRouter.ts
+++ src/routes/ArticlesRouter.ts
@@ -6,5 +6,14 @@ const articlesRouter = Router();
 articlesRouter.get('/', (req: Request, res: Response) =>
     articlesController.get(req, res)
 );
+articlesRouter.post('/', (req: Request, res: Response) =>
+    articlesController.post(req, res)
+);
+articlesRouter.patch('/:articleId', (req: Request, res: Response) =>
+    articlesController.patch(req, res)
+);
+articlesRouter.delete('/:articleId', (req: Request, res: Response) =>
+    articlesController.delete(req, res)
+);
 
 export { articlesRouter };
