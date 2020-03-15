46015133e26b3b42d4df7371dfdda0e0fbdfd6eb
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-03-15 22:42:31 +0900

    学級だよりを取得するリポジトリメソッドを作成

--- src/interface/ArticleRepository.ts
+++ src/interface/ArticleRepository.ts
@@ -1,9 +1,31 @@
 import { ArticleRepositoryInterface } from '../usecase/article/ArticleRepository';
 import { Article } from '../domain/Article';
-
+import { db } from '../../db/models/index';
+import { FindAllResult } from './repositories/articles/ArticlesQueryResult';
 export class ArticleRepository implements ArticleRepositoryInterface {
-    async findByAuthorId(): Promise<Article[]> {
-        // TODO: DBにクエリ
-        return [new Article({ id: 1, author: '山田先生', status: 2 })];
+    async findByAuthorId(authorId: number): Promise<Article[]> {
+        if (!authorId) {
+            throw new Error(`authorIdが不正です。[authorId: ${authorId}]`);
+        }
+
+        const options: any = {
+            where: { deleteFlg: false, authorId },
+            order: [['createTime', 'desc']],
+            include: [
+                {
+                    model: db.Users,
+                    required: true,
+                },
+            ],
+        };
+
+        const result: FindAllResult[] = await db.Articles.findAll(options);
+        return result.map(data => {
+            return new Article({
+                id: data.id,
+                author: data.User.name,
+                status: data.status,
+            });
+        });
     }
 }
