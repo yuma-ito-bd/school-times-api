cd0d2e2e6032a30074927177a45d3280af03601a
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-09-13 19:08:45 +0900

    学級だよりテーブルモデルクラスにクラスIDを追加

--- src/infrastructure/db/models/ArticlesTableModel.ts
+++ src/infrastructure/db/models/ArticlesTableModel.ts
@@ -6,13 +6,23 @@ import { COMMON_COLUMNS } from '../utils/CommonColumns';
 const TABLE_NAME = 'articles';
 
 export class ArticlesTableModel extends Model {
+    /** 学級だよりID */
     public id!: number;
+    /** 作成日時 */
     public createTime!: Date;
+    /** 更新日時 */
     public updateTime!: Date;
+    /** 削除フラグ */
     public deleteFlg!: boolean;
+    /** タイトル */
     public title?: string;
+    /** 本文 */
     public contents?: string;
+    /** 作者ID */
     public authorId!: number;
+    /** クラスID */
+    public classId!: number;
+    /** 状態（0: 下書き、1: 公開申請、2: 公開済み） */
     public status!: number;
 
     public static initialize(sequelize: Sequelize): void {
@@ -31,6 +41,11 @@ export class ArticlesTableModel extends Model {
                 },
                 authorId: {
                     field: 'author_id',
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                },
+                classId: {
+                    field: 'class_id',
                     type: DataTypes.INTEGER,
                     allowNull: false,
                 },
